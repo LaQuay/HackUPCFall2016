b41146ead2acc8a6c2acae9302ef9932db95ed1b
LaQuay <marcvilagomez@gmail.com>
2016-10-08 11:56:45 +0200

    Refactor, now its an Activity+Fragment app. Visual improvements on MainScreen.

--- app/src/main/java/dev/roviloapps/hackupcfall2016/MainActivity.java
+++ app/src/main/java/dev/roviloapps/hackupcfall2016/MainActivity.java
@@ -1,28 +1,19 @@
 package dev.roviloapps.hackupcfall2016;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AlertDialog;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-
-import dev.roviloapps.hackupcfall2016.controllers.AirportController;
 import dev.roviloapps.hackupcfall2016.controllers.ForecastController;
-import dev.roviloapps.hackupcfall2016.model.Airport;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,58 +22,32 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        setUpElements();
+        setUpListeners();
+
+        if (savedInstanceState != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_main, MainFragmentActivity.newInstance());
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    }
+
+    private void setUpElements() {
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+    private void setUpListeners() {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ForecastController forecastController = new ForecastController(getApplicationContext());
+                ForecastController forecastController = new ForecastController(getBaseContext());
                 forecastController.forecastRequest("35", "139");
-
-                showSearchLayout();
 
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 //        .setAction("Action", null).show();
             }
         });
-    }
-
-    private void showSearchLayout() {
-        final View dialogView = getLayoutInflater().inflate(R.layout.main_activity_dialog_search, null);
-        final AutoCompleteTextView dialogAutoComplete = (AutoCompleteTextView) dialogView.findViewById(R.id.main_activity_dialog_search_autocomplete);
-
-        final ArrayList<Airport> airportArrayList = AirportController.getInstance(this).getAirports();
-
-        ArrayAdapter<Airport> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, airportArrayList);
-        dialogAutoComplete.setAdapter(adapter);
-
-        dialogAutoComplete.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-                Airport airportSelected = (Airport) arg0.getAdapter().getItem(arg2);
-                Toast.makeText(getBaseContext(), airportSelected.getCode() + "-" + airportSelected.getName(), Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
-        alertDialog.setTitle("Search");
-        alertDialog.setMessage("Search");
-        alertDialog.setView(dialogView);
-        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "GO!",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.show();
-
-        alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
     }
 
     @Override
---
+++ app/src/main/java/dev/roviloapps/hackupcfall2016/MainFragmentActivity.java
@@ -0,0 +1,58 @@
+package dev.roviloapps.hackupcfall2016;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import dev.roviloapps.hackupcfall2016.controllers.AirportController;
+import dev.roviloapps.hackupcfall2016.model.Airport;
+
+public class MainFragmentActivity extends Fragment {
+    private static final String TAG = MainFragmentActivity.class.getSimpleName();
+    private View rootView;
+    private AutoCompleteTextView autoCompleteOriginAirport;
+
+    public static MainFragmentActivity newInstance() {
+        return new MainFragmentActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        rootView = inflater.inflate(R.layout.content_main_fragment, container, false);
+
+        setUpElements();
+        setUpListeners();
+
+        ArrayList<Airport> airportArrayList = AirportController.getInstance(getActivity()).getAirports();
+        ArrayAdapter<Airport> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_dropdown_item_1line, airportArrayList);
+        autoCompleteOriginAirport.setAdapter(adapter);
+
+        autoCompleteOriginAirport.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Airport airportSelected = (Airport) arg0.getAdapter().getItem(arg2);
+                Toast.makeText(getActivity(), airportSelected.getCode() + "-" + airportSelected.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+
+    private void setUpElements() {
+        autoCompleteOriginAirport = (AutoCompleteTextView) rootView.findViewById(R.id.content_main_origin_autocomplete);
+    }
+
+    private void setUpListeners() {
+
+    }
+}
